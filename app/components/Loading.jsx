6edd49c243e132305e11836eec2f4d19ebfd7a9c
cgreenblatt <cynthia_greenblatt@yahoo.com>
2023-01-18 14:10:43 -0800

    feat: Convert class components to function components and custom hook

--- app/components/Loading.jsx
+++ app/components/Loading.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 const styles = {
@@ -10,63 +10,34 @@ const styles = {
   textAlign: "center",
 };
 
-class Delayed extends React.Component {
-  state = {
-    show: false,
-  };
-  componentDidMount() {
-    this.timeout = window.setTimeout(() => {
-      this.setState({ show: true });
-    }, this.props.wait);
-  }
-  componentWillUnmount() {
-    window.clearTimeout(this.timeout);
-  }
-  render() {
-    return this.state.show === true ? this.props.children : null;
-  }
+function useDelay(wait) {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+      const timeout = setTimeout(() => setShow(true), wait);
+      return () => clearTimeout(timeout);
+    }, []);
+  return [show];
 }
 
-Delayed.defaultProps = {
-  wait: 300,
-};
-
-Delayed.propTypes = {
-  children: PropTypes.node.isRequired,
-  wait: PropTypes.number,
-};
-
-export default class Loading extends React.Component {
-  state = {
-    content: this.props.text,
-  };
-  componentDidMount() {
-    const { speed, text } = this.props;
-
-    this.interval = window.setInterval(() => {
-      this.state.content === text + "..."
-        ? this.setState({ content: text })
-        : this.setState(({ content }) => ({ content: content + "." }));
+export default function Loading({ speed = 300, text = 'loading'}) {
+  const [show] = useDelay(300);
+  const [dots, setDots] = useState('');
+  useEffect(() => {
+    const interval = window.setInterval(() => {
+          setDots((dots) => {
+            return dots.length === 3
+              ? '.'
+              : dots.concat('.');
+          })
     }, speed);
-  }
-  componentWillUnmount() {
-    window.clearInterval(this.interval);
-  }
-  render() {
-    return (
-      <Delayed>
-        <p style={styles}>{this.state.content}</p>
-      </Delayed>
-    );
-  }
+    return () => clearInterval(interval);
+  }, [])
+  return show
+    ? <p style={styles}>{`${text} ${dots}`}</p>
+    : null
 }
 
 Loading.propTypes = {
-  text: PropTypes.string.isRequired,
-  speed: PropTypes.number.isRequired,
-};
-
-Loading.defaultProps = {
-  text: "Loading",
-  speed: 300,
+  text: PropTypes.string,
+  speed: PropTypes.number,
 };
