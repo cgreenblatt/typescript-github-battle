c0131242b37d48a5c114bd1b8f74cd7ab8a1343c
cgreenblatt <cynthia_greenblatt@yahoo.com>
2023-01-18 15:55:10 -0800

    feat: Convert class components to function components

--- app/components/Popular.jsx
+++ app/components/Popular.jsx
@@ -1,6 +1,7 @@
-import * as React from "react";
+import React, { useEffect, useState, useReducer, useRef } from "react";
 import PropTypes from "prop-types";
 import { fetchPopularRepos } from "../utils/api";
+import Loading from './Loading';
 import Table from "./Table";
 
 function LanguagesNav({ selected, onUpdateLanguage }) {
@@ -25,53 +26,61 @@ LanguagesNav.propTypes = {
   onUpdateLanguage: PropTypes.func.isRequired,
 };
 
-export default class Popular extends React.Component {
-  state = {
-    selectedLanguage: "All",
-    repos: null,
-    error: null,
-  };
-  componentDidMount() {
-    this.updateLanguage(this.state.selectedLanguage);
-  }
-  updateLanguage = (selectedLanguage) => {
-    this.setState({
-      selectedLanguage,
-      error: null,
-    });
-
-    fetchPopularRepos(selectedLanguage)
-      .then((repos) =>
-        this.setState({
-          repos,
-          error: null,
-        })
-      )
-      .catch((error) => {
-        console.warn("Error fetching repos: ", error);
-
-        this.setState({
-          error: `There was an error fetching the repositories`,
-        });
-      });
-  };
-  render() {
-    const { selectedLanguage, repos, error } = this.state;
-
-    return (
-      <main className="stack main-stack animate-in">
-        <div className="split">
-          <h1>Popular</h1>
-          <LanguagesNav
-            selected={selectedLanguage}
-            onUpdateLanguage={this.updateLanguage}
-          />
-        </div>
-
+const initialState = {
+    loading: false,
+    repos: {},
+    error: '',
+}
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'setLoading': return {
+            ...state,
+            loading: true,
+            error: ''
+        };
+        case 'setRepo' : return {
+            loading: false,
+            repos: { ...state.repos, [action.repo.selectedLanguage]: action.repo.repos }
+        };
+        case 'setError' : return {
+            ...state,
+            error: action.error.message,
+            loading: false
+         }
+        default: throw Error(`Unknown action ${action.type} was passed to the reducer`);
+    }
+}
+export default function Popular() {
+   const [state, dispatch] = useReducer(reducer, initialState);
+   const [selectedLanguage, setSelectedLanguage] = useState('All');
+   const fetchedLanguages = useRef([]);
+   useEffect(() => {
+        if (fetchedLanguages.current.includes(selectedLanguage)) return;
+        dispatch({ type: 'setLoading' });
+        fetchPopularRepos(selectedLanguage)
+            .then((repos) => {
+                fetchedLanguages.current.push(selectedLanguage);
+                console.log('fetched languages.current', fetchedLanguages.current);
+                dispatch({ type: 'setRepo', repo: { selectedLanguage, repos } });
+            })
+            .catch((error) => {
+                console.error(error.messag);
+                dispatch({ type: 'setError', error })
+            })
+   }, [fetchedLanguages, selectedLanguage])
+   const { error, loading, repos } = state;
+   return (
+    <main className="stack main-stack animate-in">
+      <div className="split">
+        <h1>Popular</h1>
+        <LanguagesNav
+          selected={selectedLanguage}
+          onUpdateLanguage={setSelectedLanguage}
+        />
+      </div>
+        {loading && <Loading text={`Fetching ${selectedLanguage} Repositories`} /> }
         {error && <p className="text-center error">{error}</p>}
-
-        {repos && <Table repos={repos} />}
-      </main>
-    );
-  }
+        {repos[selectedLanguage] && <Table repos={repos[selectedLanguage]} />}
+    </main>
+  );
 }
